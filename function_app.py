527b12910dfec1e17491fac23fae70e30dab920c
Drew Ringo <ringodrew@gmail.com>
2024-10-23 10:51:09 -0400

    added working prediction mechanism

--- function_app.py
+++ function_app.py
@@ -1,15 +1,25 @@
 import logging
-
+import io
 import azure.functions as func
 import azurefunctions.extensions.bindings.blob as blob
+
+from resources.predict import predict_image
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.blob_trigger(
     arg_name="client", path="raw", connection="AzureWebJobsStorage"
 )
 def blob_trigger(client: blob.BlobClient):
+    blob_metadata = client.get_blob_properties()
+    blob_content = client.download_blob().read()
     logging.info(
-        f"Python blob trigger function processed blob \n"
-        f"Properties: {client.get_blob_properties()}\n"
-        f"Blob content head: {client.download_blob().read(size=1)}"
+        f"Python blob trigger function processed blob {blob_metadata} \n"
+        # f"Properties: {blob_content}\n"
     )
+
+    blob_prediction = predict_image(io.BytesIO(blob_content))
+
+    logging.info(
+        f"Blob prediction results {blob_prediction}"
+    )
+
